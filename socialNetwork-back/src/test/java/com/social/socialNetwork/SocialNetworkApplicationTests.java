a29a1c89bdf3fc11935f764e9886b91061f48960
alemadev <alessandromalanna@gmail.com>
2022-05-28 17:39:31 -0300

    let's do a fresh start

--- socialNetwork-back/src/test/java/com/social/socialNetwork/SocialNetworkApplicationTests.java
+++ socialNetwork-back/src/test/java/com/social/socialNetwork/SocialNetworkApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.social.socialNetwork;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SocialNetworkApplicationTests {
 
 	@Test
 	void contextLoads() {
