61172057322dfb525e8416cfcd0d13bc265252de
alemadev <alessandromalanna@gmail.com>
2022-07-06 14:00:34 -0300

    a

--- up.sh
+++ up.sh
@@ -5,13 +5,7 @@ function pushThis () {
 	echo "What will be the message?"
 	read -e message
 	git commit -m "$message"
-	echo "Are you sure you wanna push? (y/n)"
-	read -e yn
-	if [ "$yn" = y ] || [ "$yn" = Y ]; then
-  		git push
-	else
-  		echo "See you!"
-	fi
+  	git push
 }
 
 pushThis $1
